1eeef16531bb6278236f739db9bfb2c2e28f02bc
Ademola Adebowale <fadebowaley@gmail.com>
2023-08-03 09:46:27 +0100

    excecises on data manipulations

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Ademola Adebowale <https://github.com/fadebowaley>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
